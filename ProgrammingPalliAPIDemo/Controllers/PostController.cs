e4a89cf3869469ea07419811ac761f2fc6bfc8f2
Joynal Abedin <44470728+Joynal279@users.noreply.github.com>
2023-03-26 19:38:21 +0600

    try catch exception handled

--- ProgrammingPalliAPIDemo/Controllers/PostController.cs
+++ ProgrammingPalliAPIDemo/Controllers/PostController.cs
@@ -34,67 +34,107 @@ namespace ProgrammingPalliAPIDemo.Controllers
         [HttpGet]
         public IActionResult GetAll()
         {
-            //var posts = _dbContext.posts.ToList();
-            var posts = _postManager.GetAll().ToList(); 
-            return Ok(posts);
+            try
+            {
+                //var posts = _dbContext.posts.ToList();
+                var posts = _postManager.GetAll().ToList();
+                return Ok(posts);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            
         }
 
         [HttpGet("id")]
         public IActionResult GetById(int id)
         {
-            var post = _postManager.GetById(id: id);
-            if (post == null)
+            try
             {
-                return NotFound();
+                var post = _postManager.GetById(id: id);
+                if (post == null)
+                {
+                    return NotFound();
+                }
+                return Ok(post);
             }
-            return Ok(post);
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            
         }
 
         [HttpPost]
         public IActionResult Add(Post post)
         {
-            post.CreatedDate = DateTime.Now;
-            //_dbContext.posts.Add(post);
-            //bool isSaved = _dbContext.SaveChanges() > 0;
-
-            bool isSaved = _postManager.Add(post);
-
-            if (isSaved)
+            try
             {
-                return Created("",post);
+                post.CreatedDate = DateTime.Now;
+                //_dbContext.posts.Add(post);
+                //bool isSaved = _dbContext.SaveChanges() > 0;
+
+                bool isSaved = _postManager.Add(post);
+
+                if (isSaved)
+                {
+                    return Created("", post);
+                }
+                return BadRequest("Post save failed");
             }
-            return BadRequest("Post save failed");
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            
         }
 
         [HttpPut]
         public IActionResult Edit(Post post)
         {
-            if (post.Id == 0)
+            try
             {
-                return BadRequest("Id is missing.");
+                if (post.Id == 0)
+                {
+                    return BadRequest("Id is missing.");
+                }
+                bool update = _postManager.Update(post);
+                if (update)
+                {
+                    return Ok(post);
+                }
+                return BadRequest("Post updated failed");
             }
-            bool update = _postManager.Update(post);
-            if (update)
+            catch (Exception ex)
             {
-                return Ok(post);
+                return BadRequest(ex.Message);
             }
-            return BadRequest("Post updated failed");
+            
         }
 
         [HttpDelete]
         public IActionResult Delete(int id)
         {
-            var post = _postManager.GetById(id);
-            if (post == null)
+            try
             {
-                return NotFound();
+                var post = _postManager.GetById(id);
+                if (post == null)
+                {
+                    return NotFound();
+                }
+                bool isDelete = _postManager.Delete(post);
+                if (isDelete)
+                {
+                    return Ok("Post has been deleted");
+                }
+                return BadRequest("Post failed been deleted");
             }
-            bool isDelete = _postManager.Delete(post);
-            if (isDelete)
+            catch (Exception ex)
             {
-                return Ok("Post has been deleted");
+                return BadRequest(ex.Message);
             }
-            return BadRequest("Post failed been deleted");
+            
         }
 
     }
